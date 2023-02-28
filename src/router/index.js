4ea84d379cc55ec752c9462119ee8b4c621c2e3e
dudosyka <a.hatson22@gmail.com>
2023-02-28 16:31:18 +0300

    Router settings serve -> start in package.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LoginForm from "@/components/LoginForm.vue";
+import ProfessionView from "@/views/ProfessionView.vue";
+import MainPage from "@/components/MainPage.vue";
+
 const routes = [
   {
     path: '/',
-    name: 'login',
-    component: LoginForm
+    name: 'Main',
+    component: MainPage,
+    meta: { needAuth: true }
   },
   {
+    path: '/auth',
+    name: 'Login',
+    component: LoginForm,
+    meta: { needAuth: false }
+  },
+  {
+    path: '/profession',
+    name: 'profession',
+    component: ProfessionView,
+    meta: { needAuth: true }
   }
 ]
 
@@ -15,5 +29,20 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to) => {
+  if (to.meta.needAuth && !localStorage.getItem('token')) {
+    return {
+      path: '/auth'
+    }
+  }
+
+  if (!to.meta.needAuth && localStorage.getItem('token')) {
+    return {
+      path: '/'
+    }
+  }
+})
+
 
 export default router
